e64848452779df803162658ef27eeb831f79bc57
NeMoMinik <104688335+NeMoMinik@users.noreply.github.com>
2025-04-04 15:46:12 +0700

    Add files via upload

--- cv.py
+++ cv.py
@@ -4,66 +4,99 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from clover import long_callback
 import math
-
-# x = subprocess.Popen(["rosrun", "image_view", "video_recorder", "image:=/main_camera/image_raw"], shell=False)
-
+import numpy as np
+# x = subp3
+# 
+# rocess.Popen(["rosrun", "image_view", "video_recorder", "image:=/main_camera/image_raw"], shell=False)
+pipes = []
 rospy.init_node('cv')
 bridge = CvBridge()
 def angle(x1, y1, x2, y2):
-    return 2 * math.pi - math.atan((y2 - y1)/(x2 - x1))
-is_pipe = False
+    return math.atan((y2 - y1)/(x2 - x1))
 @long_callback
 def image_callback(data):
-    global is_pipe
-    # img = data
-    img = bridge.imgmsg_to_cv2(data, 'bgr8')  # OpenCV image                          #blur image
-    hsv = cv2.cvtColor(img[30:210, 80:240], cv2.COLOR_BGR2HSV)     
+    img = bridge.imgmsg_to_cv2(data, "bgr8")
+    img = img[30:210, 80:240]
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)     
     # hsv[:,:,2] = 255
     # light_img = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR )
-    thresh = cv2.inRange(hsv, (23, 74, 112), (41, 166, 251))   
-    cleft, _ = cv2.findContours(thresh[:, :90], cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cleft = list(filter(lambda x: cv2.contourArea(x) > 100, cleft))
-    cright, _ = cv2.findContours(thresh[:, 90:], cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cright = list(filter(lambda x: cv2.contourArea(x) > 100, cright))
-    ml, mr = [0, 0], [0, 0]
-    print(is_pipe)
-    if cleft:
-        for cnt in cleft:
-            M = cv2.moments(cnt)
-            cx = int(M['m10']/M['m00'])
-            cy = int(M['m01']/M['m00'])
-            ml[0] += cx
-            ml[1] += cy
-        ml[0] //= len(cleft)
-        ml[1] //= len(cleft)
-        img = cv2.circle(img[30:120, 80:240], tuple(ml), radius=5, color=(255, 0, 0), thickness=5)
-        cv2.imshow("xd", img)
-        for cnt in cleft:
-            if cv2.pointPolygonTest(cnt, tuple(ml), False) != 1:
-                is_pipe = True
-                return True
-    if cright:
-        for cnt in cright:
-            M = cv2.moments(cnt)
-            cx = int(M['m10']/M['m00'])
-            cy = int(M['m01']/M['m00'])
-            ml[0] += cx
-            ml[1] += cy
-        mr[0] //= len(cright)
-        mr[1] //= len(cright)
-        for cnt in cright:
-            if cv2.pointPolygonTest(cnt, tuple(mr), False) != 1:
-                is_pipe = True
-                cv2.imwrite("pipe.png", img)
-                return True
-    is_pipe = False
-    return False
+    gray = cv2.inRange(hsv, (23, 74, 112), (41, 166, 251))
+    edges = cv2.Canny(gray,50,150,apertureSize = 3)
+    # cv2.imshow("edges", edges)
+    lines = cv2.HoughLines(edges,1,np.pi/180,45)
+
+    if lines is not None:
+        lines = list(map(lambda ln: [ln[0][0], np.arctan2(np.sin(ln[0][1]), np.cos(ln[0][1]))], lines))
+        lines.sort()
+        print(lines)
+        for i in lines:
+            if math.pi - i[1] < 0.26:
+                pipe = i
+                rho,theta = pipe
+                a = np.cos(theta)
+                b = np.sin(theta)
+                x0 = a*rho
+                y0 = b*rho
+                k = b / a
+                bp = y0 - k * x0
+                break
+        else:
+            image_pub.publish(bridge.cv2_to_imgmsg(img, "bgr8"))
+            return
+        for line in lines:
+            if pipe[1] - line[1] > 0.3:
+                rho,theta = line
+                a = np.cos(theta)
+                b = np.sin(theta)
+                x0 = a*rho
+                y0 = b*rho
+                kx = b / a
+                bpx = y0 - k * x0
+                x1 = int(x0 + 1000*(-b))
+                y1 = int(y0 + 1000*(a))
+                x2 = int(x0 - 1000*(-b))
+                y2 = int(y0 - 1000*(a))
+                interx = (bpx - bp) / (k - kx)
+                intery = interx * kx + bpx
+                img = cv2.circle(img, (int(interx), int(intery)), 10, (0, 255, 255), -1) 
+
+
+                cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+    image_pub.publish(bridge.cv2_to_imgmsg(img, "bgr8"))
 image_sub = rospy.Subscriber('/main_camera/image_raw', Image, image_callback)
 image_pub = rospy.Publisher('~debug', Image)
-rospy.spin()
+
 # cap = cv2.VideoCapture("/home/clover/Desktop/xd.avi")
 # flag, img = cap.read()
 # while flag:
 #     # image_callback(img)
-#     rospy.sleep(0.03)
+#     # rospy.sleep(0.03)
 #     flag, img = cap.read()
+#     img = img[30:210, 80:240]
+#     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)     
+#     # hsv[:,:,2] = 255
+#     # light_img = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR )
+#     gray = cv2.inRange(hsv, (23, 74, 112), (41, 166, 251))
+#     edges = cv2.Canny(gray,50,150,apertureSize = 3)
+#     cv2.imshow("edges", edges)
+#     lines = cv2.HoughLines(edges,1,np.pi/180,45)
+#     print(list(map(lambda ln: np.arctan2(np.sin(ln[0][1]), np.cos(ln[0][1])), lines)))
+#     if lines is not None:
+#         lines = list(sorted(lines, key=lambda ln: np.arctan2(np.sin(ln[0][1]), np.cos(ln[0][1]))))
+        
+#         lines = (lines[0], lines)
+#         for line in lines:
+#             rho,theta = line[0]
+#             a = np.cos(theta)
+#             b = np.sin(theta)
+#             x0 = a*rho
+#             y0 = b*rho
+#             x1 = int(x0 + 1000*(-b))
+#             y1 = int(y0 + 1000*(a))
+#             x2 = int(x0 - 1000*(-b))
+#             y2 = int(y0 - 1000*(a))
+
+#             cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+#     # cv2.imshow("penis", img)
+    # cv2.waitKey(0)
+rospy.spin()
