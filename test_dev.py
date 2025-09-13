7cbd9283a547c7e83994e18ed3c080d418e8538e
Timofey Kalyak <kalyakta@yandex.ru>
2025-09-14 00:20:26 +0700

    battery percentage and voltage scanning done, todo coord scans and drawing

--- test_dev.py
+++ test_dev.py
@@ -1,38 +1,60 @@
 import threading
+import rospy
 import time
 from node import CloverAPI
+from clover import long_callback
+import sensor_msgs
+from cv_bridge import CvBridge
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 clover = CloverAPI()
-# @app.before_request
-# def get_clover_instance():
-#     global clover
-#     clover = CloverAPI()
+
+H, W = 2000, 2000
+MARGIN = 201
+
+
+def calculate_image_position():
+    aruco_map = api.get_map()["markers"]
+    left = min(aruco_map, key=lambda x: x["x"] - x["length"] / 2)
+    right = max(aruco_map, key=lambda x: x["x"] + x["length"] / 2)
+    top = max(aruco_map, key=lambda x: x["y"] + x["length"] / 2)
+    bottom = min(aruco_map, key=lambda x: x["y"] - x["length"] / 2)
+    dx = right["x"] - left["x"]  # + (right["length"] + left["length"]) / 2
+    dy = top["y"] - bottom["y"]  # + (top["length"] + bottom["length"]) / 2
+    if dx >= dy:
+        k1, k2 = right["length"] / dx, left["length"] / dx
+        dx_px = (2 * W - 4 * MARGIN) / (2 + k1 + k2)
+        scale = dx / dx_px  # doesn't really work with only one marker present, who cares?
+        (dy + (top["length"] + bottom["length"]) / 2) / (dx + (right["length"] + left["length"]) / 2)
+        margin_y = (H - (W - 2 * MARGIN) * (dy + (top["length"] + bottom["length"]) / 2) / (
+                    dx + (right["length"] + left["length"]) / 2)) / 2
+        y = top["y"] + margin_y * scale + top["length"] / 2
+        x = left["x"] - MARGIN * scale - left["length"] / 2
+        ly, lx = H * scale, W * scale
+    else:
+        k1, k2 = top["length"] / dx, bottom["length"] / dx
+        dy_px = (2 * H - 4 * MARGIN) / (2 + k1 + k2)
+        scale = dy / dy_px  # doesn't really work with only one marker present, who cares?
+
+        margin_x = (W - (H - 2 * MARGIN) * (dx + (right["length"] + left["length"]) / 2) / (
+                    dy + (top["length"] + bottom["length"]) / 2)) / 2
+        y = top["y"] + MARGIN * scale + top["length"] / 2
+        x = left["x"] - margin_x * scale - left["length"] / 2
+        ly, lx = H * scale, W * scale
+    return x, y, lx, ly
+
 
 # --- serve frontend ---
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 # Background thread for continuous updates
 def map_updater():
-    # class Data:
-    #     def __init__(self, id, length, pose):
-    #         self.id = id,
-    #         self.length = length,
-    #         self.pose = pose
-    #         self.x = pose[0]
-    #         self.y = pose[1]
-    #         self.z = pose[2]
-
-    # class Zaglushka:
-    #     def __init__(self, markers):
-    #         self.markers = markers
-    # data_pass = [Data(0, 1, (0, 0, 0)), Data(1, 1, (1, 1, 1)), Data(2, 1, (3, 3, 3))]
-    # state = Zaglushka(data_pass)
     state = clover.get_map()
     while True:
         res = []
@@ -46,7 +68,8 @@ def map_updater():
             }
             res.append(marker)
         socketio.emit('map', {"markers": res})
-        time.sleep(0.2)  # 5 updates per second
+        time.sleep(10)  # 5 updates per second
+
 
 # Start background thread once when server starts
 @socketio.on('connect')
@@ -54,21 +77,27 @@ def start_updates():
     global updater_thread
     if not hasattr(start_updates, "started"):
         updater_thread = threading.Thread(target=map_updater, daemon=True)
+        updater_thread.start()
+
         battery_thread = threading.Thread(target=battery_updater, daemon=True)
         battery_thread.start()
-        updater_thread.start()
+
+        coords_thread = threading.Thread(target=get_coords, daemon=True)
+        coords_thread.start()
+
         start_updates.started = True
 
 
+@socketio.on('get_coords')
+def get_coords():
+    while True:
+        telem = clover.get_telemetry(frame_id='aruco_map')
+        emit('coords', {'x': telem.x, 'y': telem.y, 'z': telem.z})
+        time.sleep(0.2)
+
+
 def battery_updater():
-    class Battery:
-        def __init__(self, vol, per):
-            self.voltage = vol
-            self.percentage = per
- 
     bat = clover.get_battery_state()
-    # bat = Battery(3, 50)
-    # mavros = clover.get_state()
     while True:
         socketio.emit('state', {"voltage": bat.voltage, "percentage": bat.percentage})
         time.sleep(1)
